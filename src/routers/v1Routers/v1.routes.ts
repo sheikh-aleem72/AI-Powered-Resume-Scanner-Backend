2092b602fa4a5ba230d0694c4b16d21b4b216019
aleem <sheikhaleem363@gmail.com>
2025-10-21 12:38:35 +0530

    feat: Add routes for resume controllers

--- src/routers/v1Routers/v1.routes.ts
+++ src/routers/v1Routers/v1.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRouter from './user.routes';
 import cloudinaryRouter from './cloudinary.routes';
+import resumeRouter from './resume.routes';
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.use('/user', userRouter);
 
 // Cloudinary route
 router.use('/cloudinary', cloudinaryRouter);
+
+// Resume route
+router.use('/resume', resumeRouter);
 
 export default router;
