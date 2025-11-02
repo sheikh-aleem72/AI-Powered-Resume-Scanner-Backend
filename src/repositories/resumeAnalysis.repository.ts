547eae77dcddf593cc6f2e4d6fa9c45c3f53f7c2
aleem <sheikhaleem363@gmail.com>
2025-11-02 12:40:23 +0530

    feat(repositories): implement ResumeAnalysis repository with CRUD operations

---
+++ src/repositories/resumeAnalysis.repository.ts
@@ -0,0 +1,25 @@
+import { ResumeAnalysisModel, IResumeAnalysis } from '../schema/resumeAnalysis.model';
+
+export const createResumeAnalysis = async (data: Partial<IResumeAnalysis>) => {
+  return await ResumeAnalysisModel.create(data);
+};
+
+export const findAnalysisByResumeAndJob = async (resumeId: string, jobId: string) => {
+  return await ResumeAnalysisModel.findOne({ resumeId, jobId });
+};
+
+export const findAnalysisById = async (id: string) => {
+  return await ResumeAnalysisModel.findById(id);
+};
+
+export const updateResumeAnalysis = async (id: string, updateData: Partial<IResumeAnalysis>) => {
+  return await ResumeAnalysisModel.findByIdAndUpdate(id, updateData, { new: true });
+};
+
+export const getAllAnalyses = async () => {
+  return await ResumeAnalysisModel.find().populate('resumeId jobId');
+};
+
+export const deleteAnalysis = async (id: string) => {
+  return await ResumeAnalysisModel.findByIdAndDelete(id);
+};
